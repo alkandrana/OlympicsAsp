7896dd6299ce339ee692bd8fa48414f894105742
Rosa Myers <larosaardente@gmail.com>
2026-06-01 09:03:53 -0700

    Initial commit.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OlympicsAsp.Models;
+namespace OlympicsAsp.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Goal> Goals => Set<Goal>();
+}
---
+++ Models/Goal.cs
@@ -0,0 +1,9 @@
+﻿namespace OlympicsAsp.Models;
+
+public class Goal
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+    public DateOnly Deadline { get; set; }
+}
