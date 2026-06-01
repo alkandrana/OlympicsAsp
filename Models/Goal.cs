8b8a80e9850b1d4dcb031bdf6d39227928a07c83
Rosa Myers <larosaardente@gmail.com>
2026-06-01 10:41:13 -0700

    Project reset.

--- Models/Goal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OlympicsAsp.Models;
-
-public class Goal
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Today);
-    public DateOnly Deadline { get; set; }
-}
